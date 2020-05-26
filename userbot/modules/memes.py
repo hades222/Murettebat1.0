0f759270127d5852293b84d216bc989461b990bb
xNull <65774707+XNulI@users.noreply.github.com>
2020-05-26 22:46:25 +0300

    Hack Meme

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -728,6 +728,32 @@ async def fuk(event):
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 4])
 
+@register(outgoing=True, pattern="^.hack")
+async def port_hack(event):
+    if event.fwd_from:
+        return
+
+    animation_interval = 3
+    animation_ttl = range(0, 11)
+    #input_str = event.pattern_match.group(1)
+    #if input_str == "hack":
+    await event.edit("Hacking..")
+    animation_chars = [
+            "`Connecting To Hacked Private Server...`",
+            "`Target Selected.`",
+            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
+            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
+            "`Hacking... 100%\n█████████HACKED███████████ `",
+            "`Targeted Account Hacked by Turhan...\n\n Nude göndermezsen hesabını sileriz..`"
+        ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 11])
                       
 @register(outgoing=True, pattern="^.otuzbir")
 async def otuzbir(event):
